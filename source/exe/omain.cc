9b4fb3b85768a1548f111d7affe0eee03e910b79
Matt Rice <mrice32@users.noreply.github.com>
2017-05-29 19:43:29 -0400

    WIP

--- source/exe/omain.cc
+++ source/exe/omain.cc
@@ -2,22 +2,23 @@
 
 #include <iostream>
 
-class Logger : google::LogSink {
-  void send(LogSeverity severity, const char* full_filename,
+class Logger : public google::LogSink {
+  void send(google::LogSeverity severity, const char* full_filename,
                     const char* base_filename, int line,
                     const struct ::tm* tm_time,
                     const char* message, size_t message_len) override {
     std::cout << "sink call" << std::endl;
-    std::cout << ToString(severity, file, line, tm_time, message, message_len) << std::endl;
+    (void) full_filename;
+    std::cout << ToString(severity, base_filename, line, tm_time, message, message_len) << std::endl;
   }
-}
+};
 
 
 int main(int argc, char** argv) {
   (void) argc;
   google::InitGoogleLogging(argv[0]);
   Logger logger;
-    LOG_TO_SINK_BUT_NOT_TO_LOGFILE(logger, ERROR) << "hello world";
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(&logger, ERROR) << "hello world";
   LOG(ERROR) << "hey earth";
   return 0;
 }
