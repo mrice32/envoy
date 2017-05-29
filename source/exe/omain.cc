2ff28870562be1dcbef0ea0a1163611086661522
Matt Rice <mrice32@users.noreply.github.com>
2017-05-29 19:25:26 -0400

    WIP

--- source/exe/omain.cc
+++ source/exe/omain.cc
@@ -1,8 +1,8 @@
 #include "glog/logging.h"
 
 int main(int argc, char** argv) {
-
-  google::nitGoogleLogging(argv[0]);
+  (void) argc;
+  google::InitGoogleLogging(argv[0]);
   LOG(ERROR) << "hey earth";
   return 0;
 }
